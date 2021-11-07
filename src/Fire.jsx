15cc4ab665263ed5517d1854dc08674635f50779
priyan <45042030+priyan1995@users.noreply.github.com>
2021-11-07 18:41:51 +0530

    firebase added

---
+++ src/Fire.jsx
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDzPpbMmZaxE_tNrUbaNGBQtxqEHhNwsFI",
+    authDomain: "user-register-e6f74.firebaseapp.com",
+    databaseURL: "https://user-register-e6f74-default-rtdb.firebaseio.com",
+    projectId: "user-register-e6f74",
+    storageBucket: "user-register-e6f74.appspot.com",
+    messagingSenderId: "132543795830",
+    appId: "1:132543795830:web:ae096cdf446aefe76f8557"
+  };
+  
+ 
+ export const fire = firebase.initializeApp(firebaseConfig);
+
